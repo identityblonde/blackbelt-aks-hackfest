59f776bd4d006e867d42a067e2f2e8d467dcf9ad
identityblonde <45366235+identityblonde@users.noreply.github.com>
2018-12-05 17:12:05 -0600

    Update Footer.vue
    Updated from Azure to Erin (Identity Blonde!) Global Blackbelt Team

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Erin (Identity Blonde!!) Global Blackbelt Team
+        Erin's (Identity Blonde!!) Global Blackbelt Team
       </div>
       <div class="col-lg-6">
       </div>
