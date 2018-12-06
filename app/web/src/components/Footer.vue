ba9ac5889a8030f06beced3e78b1e09e279d70f2
identityblonde <45366235+identityblonde@users.noreply.github.com>
2018-12-06 15:41:58 -0600

    Update Footer.vue

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Identity Blonde says hello!!
+        Identity Blonde says hola!
       </div>
       <div class="col-lg-6">
       </div>
