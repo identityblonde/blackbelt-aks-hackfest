fe2c83c8d12c765f5fe9a6ab7c61ecb07e453da3
identityblonde <45366235+identityblonde@users.noreply.github.com>
2018-12-06 15:17:12 -0600

    update

--- app/web/src/components/Footer.vue
+++ app/web/src/components/Footer.vue
@@ -14,7 +14,7 @@
       <div class="col-lg-6">
       </div>
       <div class="col-lg-12 credits">
-        Erin McGovern aka Identity Blonde!!!
+        Identity Blonde says hello!!
       </div>
       <div class="col-lg-6">
       </div>
